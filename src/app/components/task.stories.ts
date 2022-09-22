f08c62d28e5df20294db18635338de6ed2b86856
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2022-09-22 22:36:55 +0100

    single component

---
+++ src/app/components/task.stories.ts
@@ -0,0 +1,45 @@
+import { Meta, Story } from '@storybook/angular';
+
+import { TaskComponent } from './task.component';
+
+export default {
+  component: TaskComponent,
+  title: 'Task',
+  argTypes: {
+    pinTask: {
+      action: 'pinTask',
+    },
+    archiveTask: {
+      action: 'archiveTask',
+    },
+  },
+} as Meta;
+
+const Template: Story = (args) => ({
+  props: args,
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  task: {
+    id: '1',
+    title: 'Test Task',
+    state: 'TASK_INBOX',
+  },
+};
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+  task: {
+    ...Default.args['task'],
+    state: 'TASK_PINNED',
+  },
+};
+
+export const Archived = Template.bind({});
+Archived.args = {
+  task: {
+    ...Default.args['task'],
+    state: 'TASK_ARCHIVED',
+  },
+};
